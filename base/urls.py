e9baa932484b566ae7f15e3efd6db775f5f2762e
Karmel920 <mikolajpe2@gmail.com>
2023-02-24 00:54:10 +0100

    Add new urls to routing

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_view, name='register'),
+
     path('', views.home, name="home"),
     path('project/<str:pk>', views.project, name="project"),
 
     path('create-project/', views.create_project, name="create-project"),
     path('update-project/<str:pk>/', views.update_project, name="update-project"),
     path('delete-project/<str:pk>/', views.delete_project, name="delete-project"),
+    path('delete-message/<str:pk>/', views.delete_message, name="delete-message"),
 ]
