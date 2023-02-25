ab0c3f8052ea3291625da1d74400e4fdd2430a84
Karmel920 <mikolajpe2@gmail.com>
2023-02-26 00:10:08 +0100

    Update urls

--- base/urls.py
+++ base/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('update-project/<str:pk>/', views.update_project, name="update-project"),
     path('delete-project/<str:pk>/', views.delete_project, name="delete-project"),
     path('delete-message/<str:pk>/', views.delete_message, name="delete-message"),
+
+    path('delete-message/', views.update_user, name="update-user"),
 ]
