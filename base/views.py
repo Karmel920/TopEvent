a2e7de01ea2f59e5f8a03bf687ae826fecc20f63
Karmel920 <mikolajpe2@gmail.com>
2023-02-27 18:59:16 +0100

    Mobile views, update user view, registartion with details

--- base/views.py
+++ base/views.py
@@ -5,8 +5,8 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Project, Topic, Message
-from .forms import ProjectForm
+from .models import Project, Topic, Message, UserDetails
+from .forms import ProjectForm, UserForm, UserDetailsForm, UserDetailsCreationForm
 
 
 def login_view(request):
@@ -37,6 +37,7 @@ def logout_user(request):
 
 
 def register_view(request):
+    user = request.user
     form = UserCreationForm()
 
     if request.method == 'POST':
@@ -46,12 +47,27 @@ def register_view(request):
             user.username = user.username.lower()
             user.save()
             login(request, user)
-            return redirect('home')
+            return redirect('register-details')
         else:
             messages.error(request, 'Something went wrong during registration')
 
     context = {'form': form}
     return render(request, 'base/login_register.html', context)
+
+
+def create_user_details(request):
+    form = UserDetailsCreationForm()
+    if request.method == 'POST':
+        user = request.user
+        UserDetails.objects.create(
+            user=user,
+            name=request.POST.get('name'),
+            email=request.POST.get('email')
+        )
+        return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/register_details.html', context)
 
 
 def home(request):
@@ -63,16 +79,18 @@ def home(request):
     )
 
     all_projects = Project.objects.all()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     project_count = projects.count()
     comments = Message.objects.filter(Q(project__topic__name__icontains=q))
 
-    context = {'projects': projects, 'topics': topics, 'project_count': project_count, 'comments': comments, 'all_projects': all_projects}
+    context = {'projects': projects, 'topics': topics, 'project_count': project_count, 'comments': comments,
+               'all_projects': all_projects}
     return render(request, 'base/home.html', context)
 
 
 def project(request, pk):
     project = Project.objects.get(id=pk)
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
     comments = project.message_set.all()
     participants = project.participants.all()
 
@@ -95,7 +113,8 @@ def user_profile(request, pk):
     all_projects = Project.objects.all()
     comments = user.message_set.all()
     topics = Topic.objects.all()
-    context = {'user': user, 'projects': projects, 'comments': comments, 'topics': topics, 'all_projects': all_projects}
+    context = {'user': user, 'projects': projects, 'comments': comments, 'topics': topics,
+               'all_projects': all_projects}
     return render(request, 'base/profile.html', context)
 
 
@@ -165,4 +184,27 @@ def delete_message(request, pk):
 
 @login_required(login_url='/login')
 def update_user(request):
-    return render(request, 'base/update-user.html')
+    user = request.user
+    form = UserDetailsForm(instance=user.userdetails)
+
+    if request.method == 'POST':
+        form = UserDetailsForm(request.POST, request.FILES, instance=user.userdetails)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form': form}
+    return render(request, 'base/update-user.html', context)
+
+
+def topics_view(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+
+def activity_view(request):
+    comments = Message.objects.all()
+    context = {'comments': comments}
+    return render(request, 'base/activity.html', context)
